4e06cd27d08d36bbc7d6d1eb0d511799e70ab51f
mohamedsiddhik <117736610+mohamedsiddhik@users.noreply.github.com>
2022-11-22 13:24:46 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,18 @@
+
+
+public  class Datatypes
+{
+    public  static void main(String args[])
+    {
+        int age = 23;
+        float percentage = 76.8f;
+        char Scheme = 'A';
+        boolean isPass = false;
+
+        System.out.println("Age : " + age);
+        System.out.println("Percentage : "  + percentage) ;
+        System.out.println("Scheme : " +Scheme);
+        System.out.println("Pass : " + isPass);
+
+    }
+}
